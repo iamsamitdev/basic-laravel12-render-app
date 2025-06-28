5796b956a139996fdefea6390674399e59c5000a
Samit Koyom <samitkoyom@gmail.com>
2025-06-28 17:16:32 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
+export default defineConfig(() => {
 
     return {
         plugins: [
