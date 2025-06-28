9564e95bb49a682a4125d8a8c313c84b27a67feb
Samit Koyom <samitkoyom@gmail.com>
2025-06-28 16:14:41 +0700

    update vite.config path#1

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,27 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-
-    const env = loadEnv(mode, process.cwd())
+export default defineConfig(() => {
 
     return {
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: false,
+                refresh: true,
             }),
             tailwindcss(),
         ],
-        base: '/build/', // หรือปล่อยเป็นค่าว่าง '' เพือให้ vite ใช้ relative path
+        server: {
+            https: true, // Enable HTTPS
+        },
+        base: '/build/', // หรือปล่อยว่างเป็น '' เพื่อให้ Vite ใช้ relative path
         build: {
             manifest: true,
             manifestFileName: 'manifest.json',
             outDir: 'public/build',
             emptyOutDir: true,
+            assetsDir: '', // ทำให้ assets ไม่อยู่ในโฟลเดอร์ย่อย
             rollupOptions: {
                 input: [
                     'resources/css/app.css',
